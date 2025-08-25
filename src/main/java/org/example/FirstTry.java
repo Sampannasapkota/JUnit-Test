087faeed62ac6eeea18177987ee7825bf54ef61d
Sampannasapkota <henryatokpas12@gmail.com>
2025-08-25 23:18:43 +0545

    This is my initial commit using JUnit test

---
+++ src/main/java/org/example/FirstTry.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class FirstTry {
+    public String number(int a){
+        if(a==5){
+            return "Sampanna Sapkota is java developer";
+        }
+
+        else {
+            return "Sorry!";
+        }
+    }
+}
---
+++ src/test/java/org/example/FirstTryTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class FirstTryTest {
+    @Test
+    void numberTest(){
+        FirstTry obj1= new FirstTry();
+        String result= obj1.number(5);
+        Assertions.assertEquals("Sampanna Sapkota is java developer",result);
+    }
+
+    @Test
+    void numberExtraTest(){
+        FirstTry obj1= new FirstTry();
+        String result= obj1.number(2);
+        Assertions.assertEquals("Sorry!",result);
+    }
+
+    @Test
+    void numberNegativeTest(){
+        FirstTry obj1= new FirstTry();
+        String result= obj1.number(-6);
+        Assertions.assertEquals("Sorry!",result);
+    }
+
+}
